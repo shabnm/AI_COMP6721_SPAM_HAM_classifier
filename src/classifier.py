170b3a58074eeb9413830bf3655787079eb88008
shabnm <Yococino123@>
2019-04-03 15:58:50 -0400

    inference of the model

--- src/classifier.py
+++ src/classifier.py
@@ -1,8 +1,25 @@
+import math
+
 from src.conditional_probability import ConditionalProbability
 from src.data_provider import DataProvider
 from src.inverted_index import InvertedIndex
 
 
-class classifier():
-    inverted_index = InvertedIndex.create_inverted_index(DataProvider())
-    model = ConditionalProbability.calc_probability(inverted_index, True)
+class Classifier:
+
+    @staticmethod
+    def classify(k_prob, word_count, smoothing):
+        labels=list(k_prob.keys())
+        results={}
+
+        for label in labels:
+            result = 0
+            for word in word_count.keys():
+                if word in k_prob[label]:
+                    result+=math.log(word_count[word] * k_prob[label][word])
+            results[label] = result
+        result_label = labels[0]
+        for label in labels:
+            if results[label]> results[result_label]:
+                result_label = label
+        return result_label , results
--- test/classifier_test.py
+++ test/classifier_test.py
@@ -1,3 +1,6 @@
+import re
+
+from src.classifier import Classifier
 from src.conditional_probability import ConditionalProbability
 from src.data_provider import DataProvider
 from src.inverted_index import InvertedIndex
@@ -18,3 +21,17 @@ class ClassifierTest(unittest.TestCase):
         inverted_index = InvertedIndex.create_inverted_index(DataProvider())
         k_prob = ConditionalProbability.calc_probability(inverted_index, True)
         ModelDumper.dump(inverted_index, k_prob)
+
+        provider = DataProvider()
+        files = provider.get_files('test')
+        for label in files:
+            for file in files[label]:
+                word_count = {}
+                with open(file, encoding='latin-1', mode='r') as f:
+                    clean_text = re.split("[^a-zA-Z]+", f.read().lower())
+                    for word in clean_text:
+                        if word not in word_count:
+                            word_count[word] = 0
+                        word_count[word] += 1
+                    result_label, probs = Classifier.classify(k_prob, word_count, False)
+                    print(label,result_label, probs)
